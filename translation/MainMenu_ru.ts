eaf30ca2172fbad33d670c01545b23036e1fcec7
Ivan Vorobyev <ivorob@gmail.com>
2020-08-30 14:50:02 +0300

    Add russian translation file

---
+++ translation/MainMenu_ru.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainMenu</name>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="12"/>
+        <source>&amp;File</source>
+        <translation type="unfinished">&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="15"/>
+        <source>&amp;New</source>
+        <translation type="unfinished">Создать</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="22"/>
+        <source>&amp;Open...</source>
+        <translation type="unfinished">&amp;Открыть...</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="29"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished">&amp;Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="42"/>
+        <source>Save &amp;As...</source>
+        <translation type="unfinished">&amp;Сохранить как...</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="51"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">В&amp;ыход</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="58"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainMenu.qml" line="61"/>
+        <source>&amp;About</source>
+        <translation type="unfinished">&amp;О программе</translation>
+    </message>
+</context>
+</TS>
