6f50f665650d358fd33900eef06eaa82c0c0135d
Ivan Vorobyev <ivorob@gmail.com>
2020-09-22 22:49:44 +0300

    Add ability to load theme from file

--- src/ThemeDispatcher.cpp
+++ src/ThemeDispatcher.cpp
@@ -1,51 +1,101 @@
+#include <QJsonDocument>
+#include <QFile>
+
 #include "ThemeDispatcher.h"
 
 ThemeDispatcher::ThemeDispatcher(QObject *parent)
-    : QObject(parent)
+    : QObject(parent),
+      themeName("Default")
 {
-    auto& todoListView = themeItems["todoListView"];
-    todoListView.insert("selection.color", "lightsteelblue");
+    QFile themeFile(":/themes/Default.theme");
+    if (themeFile.open(QIODevice::ReadOnly)) {
+        loadTheme(&themeFile);
+    }
+}
 
-    auto& root = themeItems["root"];
-    root.insert("color", "white");
+QString
+ThemeDispatcher::getThemeName() const
+{
+    return this->themeName;
+}
 
-    auto& expectations = themeItems["expectations"];
-    expectations.insert("color", "white");
-    expectations.insert("selection.color", "lightsteelblue");
-    expectations.insert("workzone.color", "white");
-    //context menu
-    expectations.insert("contextMenu.color", "white");
-    expectations.insert("contextMenu.border", "black");
-    expectations.insert("contextMenu.selection.color", "#F5F5F5");
-    expectations.insert("contextMenu.textColor", "black");
+void
+ThemeDispatcher::setThemeName(const QString& name)
+{
+    this->themeName = name;
+}
 
-    auto& reality = themeItems["reality"];
-    reality.insert("color", "white");
-    reality.insert("selection.color", "lightsteelblue");
-    reality.insert("workzone.color", "white");
-    //context menu
-    reality.insert("contextMenu.color", "white");
-    reality.insert("contextMenu.border", "black");
-    reality.insert("contextMenu.selection.color", "#F5F5F5");
-    reality.insert("contextMenu.textColor", "black");
+bool
+ThemeDispatcher::loadTheme(QIODevice *device)
+{
+    if (device) {
+        QJsonDocument document = QJsonDocument::fromJson(device->readAll());
+        if (!document.isNull() && document.isObject()) {
+            QJsonObject root = document.object();
 
-    auto& fileMenu = themeItems["fileMenu"];
-    fileMenu.insert("color", "white");
-    fileMenu.insert("border", "black");
-    fileMenu.insert("selection.color", "#F5F5F5");
-    fileMenu.insert("textColor", "black");
+            QString name;
+            auto items = readThemeProperties(root, name);
+            if (!items.isEmpty()) {
+                setThemeName(name);
 
-    auto& languageMenu = themeItems["languageMenu"];
-    languageMenu.insert("color", "white");
-    languageMenu.insert("border", "black");
-    languageMenu.insert("selection.color", "#F5F5F5");
-    languageMenu.insert("textColor", "black");
+                themeItems = items;
+            }
 
-    auto& helpMenu = themeItems["helpMenu"];
-    helpMenu.insert("color", "white");
-    helpMenu.insert("border", "black");
-    helpMenu.insert("selection.color", "#F5F5F5");
-    helpMenu.insert("textColor", "black");
+            return true;
+        }
+    }
+
+    return false;
+}
+
+ThemeDispatcher::ThemeItems
+ThemeDispatcher::readThemeProperties(const QJsonObject& root, QString& themeName) const
+{
+    QJsonValue name = root.value("name");
+    if (name.isString()) {
+        QJsonValue style = root.value("style");
+        if (style.isObject()) {
+            themeName = name.toString();
+            return readStyleData(style.toObject());
+        }
+    }
+
+    return {};
+}
+
+ThemeDispatcher::ThemeItems
+ThemeDispatcher::readStyleData(const QJsonObject& style) const
+{
+    ThemeItems items;
+
+    for (auto it = style.constBegin(); it != style.constEnd(); ++it)
+    {
+        const auto& value = it.value();
+        if (value.isObject()) {
+            readObject(items, it.key(), "", value.toObject());
+        }
+    }
+
+    return items;
+}
+
+void
+ThemeDispatcher::readObject(ThemeItems& items, const QString& id, const QString& key, const QJsonObject& currentObject) const
+{
+    for (auto it = currentObject.constBegin(); it != currentObject.constEnd(); ++it)
+    {
+        QString currentKey = it.key();
+        if (!key.isEmpty()) {
+            currentKey = key + "." + it.key();
+        }
+
+        const auto& value = it.value();
+        if (value.isString()) {
+            items[id].insert(currentKey, value.toString());
+        } else if (value.isObject()) {
+            readObject(items, id, currentKey, value.toObject());
+        }
+    }
 }
 
 QString
--- tests/ThemeDispatcherTests.cpp
+++ tests/ThemeDispatcherTests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <QBuffer>
 
 #include "ThemeDispatcher.h"
 
@@ -14,18 +15,52 @@ TEST(ThemeDispatcher, getStringPropertyForInvalidPropertyNameTest)
     ASSERT_EQ(QString(), themeDispatcher.getStringProperty("todoListView", "invalidPropertyName"));
 }
 
-TEST(ThemeDispatcher, getStringPropertyTest)
+TEST(ThemeDispatcher, getThemeNameTest)
 {
     ThemeDispatcher themeDispatcher;
-    ASSERT_EQ(QString("lightsteelblue"), themeDispatcher.getStringProperty("todoListView", "selection.color"));
+    ASSERT_EQ(QString("Default"), themeDispatcher.getThemeName());
+}
+
+TEST(ThemeDispatcher, setThemeNameTest)
+{
+    ThemeDispatcher themeDispatcher;
+    themeDispatcher.setThemeName("Light");
+    ASSERT_EQ(QString("Light"), themeDispatcher.getThemeName());
+}
+
+TEST(ThemeDispatcher, readStyleDataFromStream)
+{
+    QBuffer buffer;
+    buffer.open(QBuffer::ReadWrite);
+    buffer.seek(0);
+    buffer.write(
+R"({
+    "name" : "Light",
+    "style" : {
+        "root" : {
+            "color" : "white"
+        },
+        "expectations" : {
+            "color" : "white",
+            "workzone" : {
+                "color" : "white"
+            },
+            "selection" : {
+                "color" : "lightsteelblue"
+            }
+        }
+    }
+})");
+    buffer.seek(0);
+
+    ThemeDispatcher themeDispatcher;
+    ASSERT_TRUE(themeDispatcher.loadTheme(&buffer));
+    ASSERT_EQ(QString("Light"), themeDispatcher.getThemeName());
 
     ASSERT_EQ(QString("white"), themeDispatcher.getStringProperty("root", "color"));
 
     ASSERT_EQ(QString("white"), themeDispatcher.getStringProperty("expectations", "color"));
     ASSERT_EQ(QString("white"), themeDispatcher.getStringProperty("expectations", "workzone.color"));
     ASSERT_EQ(QString("lightsteelblue"), themeDispatcher.getStringProperty("expectations", "selection.color"));
-
-    ASSERT_EQ(QString("white"), themeDispatcher.getStringProperty("expectations", "color"));
-    ASSERT_EQ(QString("white"), themeDispatcher.getStringProperty("expectations", "workzone.color"));
-    ASSERT_EQ(QString("lightsteelblue"), themeDispatcher.getStringProperty("expectations", "selection.color"));
+    ASSERT_EQ(QString(), themeDispatcher.getStringProperty("reality", "selection.color"));
 }
